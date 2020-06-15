8f503918aae3c0ed38dc86224b7c2fcac91a0124
Payrav <payrav-343@mail.ru>
2020-06-15 17:07:13 +0500

    Added  OrderController

--- CarStoreWebApp/Controllers/ClientCartController.cs
+++ CarStoreWebApp/Controllers/ClientCartController.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarStoreWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarStoreWebApp.Controllers
 {
-    public class ClientCartController : Controller
+    public class ClientCarController : Controller
     {
+        public DataContext _context;
+        public ClientCarController(DataContext context)
+        {
+            this._context = context;
+        }
+        // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Categories = _context.Categories.ToList();
+            var li = _context.Cars.OrderByDescending(p => p).Include(p => p.Category).ToList();
+            return View(li);
+        }
+
+        public async Task<IActionResult> AddCart(int id)
+        {
+            var product = await _context.Cars.FirstOrDefaultAsync(p => p.Id == id);
+
+
+
+            _context.Carts.Add(new Cart { Item = product.Id });
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("CartList");
+        }
+
+        public async Task<IActionResult> CartList()
+        {
+            var cartList = await _context.Carts.ToListAsync();
+            foreach (var cartProduct in cartList)
+            {
+                cartProduct.Cars = await _context.Cars.Where(p => p.Id == cartProduct.carId).ToListAsync();
+            }
+            return View(cartList);
         }
     }
 }
---
+++ CarStoreWebApp/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarStoreWebApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddOrder(string model)
+        {
+            return View();
+        }
+    }
+}
